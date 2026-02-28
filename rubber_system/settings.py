35dd79f3d387524f3172f8699856e6adccfe2be0
ashishprince <ashishprince00789@gmail.com>
2026-03-01 01:11:22 +0530

    Add whitenoise to INSTALLED_APPS

--- rubber_system/settings.py
+++ rubber_system/settings.py
@@ -46,6 +46,7 @@ ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
